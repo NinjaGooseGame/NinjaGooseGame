ffbed27bafd38cc90f27353296928fbd0ddd2fe5
NinjaGooseGame <bengtsonjohannes@gmail.com>
2025-10-20 23:15:40 +0200

    Update base-path.js

--- js/base-path.js
+++ js/base-path.js
@@ -1,6 +1,18 @@
 (function(){
-  var base = document.createElement('base');
-  // Byt "goosehunt" till namnet på ditt GitHub Pages-repo
-  base.href = location.hostname.endsWith('github.io') ? '/goosehunt/' : '/';
-  document.head.appendChild(base);
+  // Är vi på en github.io-domän?
+  var isGh = /\.github\.io$/i.test(location.hostname);
+
+  // Första path-segmentet (t.ex. "goosehunt" på /goosehunt/index.html)
+  var seg = location.pathname.split('/').filter(Boolean)[0] || '';
+
+  // Hitta/Skapa <base>
+  var baseEl = document.querySelector('base') || document.createElement('base');
+
+  // Logik:
+  // - User site (ninjagoosegame.github.io): location.pathname är "/" => seg = "" => base "/"
+  // - Project site (...github.io/goosehunt/): seg = "goosehunt" => base "/goosehunt/"
+  // - Custom domain: isGh = false => base "/"
+  baseEl.href = (isGh && seg ? '/' + seg + '/' : '/');
+
+  if(!baseEl.parentNode) document.head.prepend(baseEl);
 })();
