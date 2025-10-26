7fd4e5441f10c5c79ceaa8c160483da54149cd4a
NinjaGooseGame <bengtsonjohannes@gmail.com>
2025-10-26 22:50:56 +0100

    fix

--- js/base-path.js
+++ js/base-path.js
@@ -1,6 +1,20 @@
 (function(){
+  // Skapa <base>-tagg
   var base = document.createElement('base');
-  // Om din sida ligger på https://<username>.github.io/ (dvs repo = användarnamn)
-  base.href = '/';
-  document.head.appendChild(base);
+
+  // Kolla om vi kör på GitHub Pages (domän slutar på github.io)
+  var isGitHub = location.hostname.endsWith('github.io');
+
+  // Försök hitta repo-namnet från URL:en (ex: /NinjaGooseGame/)
+  var match = location.pathname.match(/^\/([^/]+)\//);
+  var repo = isGitHub && match ? match[1] : '';
+
+  // Bygg dynamisk bas
+  base.href = location.origin + (repo ? '/' + repo + '/' : '/');
+
+  // Sätt in i <head>
+  document.head.prepend(base);
+
+  // Debug
+  console.log('[base-path] base.href =', base.href);
 })();
